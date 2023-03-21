6e6dc28f2d2f7ed131d2e9775cbe88075c1b59da
GWANGUIAN <bbaa3218@gmail.com>
2023-03-21 19:22:25 +0900

    feat: demo code for animejs

--- src/pages/frameworks/animejs.tsx
+++ src/pages/frameworks/animejs.tsx
@@ -2,26 +2,46 @@ import dynamic from 'next/dynamic';
 
 const Anime = dynamic(() => import('react-anime'), { ssr: false });
 import { css } from '@emotion/react';
-import anime from 'animejs';
 
+// import anime from 'animejs';
 import Navigation from '../../components/layouts/Navigation';
 
-const colors = ['blue', 'green', 'red'];
+const container = css`
+  /* display: flex;
+  flex-direction: column;
+  justify-content: center;
+  overflow: hidden; */
+  overflow: hidden;
+`;
+
+const colors = ['blue', 'green', 'red', 'orange', 'yellow'];
 
 const colorBox = (color: string) => css`
   width: 100px;
   height: 100px;
+  margin: 10px;
   background-color: ${color};
+  border-radius: 50%;
 `;
 
 const Animejs = () => (
   <>
     <Navigation />
-    <Anime delay={anime.stagger(100)} scale={[0.1, 0.9]}>
-      {colors.map((color, i) => (
-        <div key={i} css={colorBox(color)} />
-      ))}
-    </Anime>
+    <div css={container}>
+      <Anime
+        easing="easeOutElastic"
+        loop={true}
+        duration={1000}
+        direction="alternate"
+        delay={(_: unknown, index: number) => index * 240}
+        translateX="100px"
+        scale={[0.1, 1]}
+      >
+        {colors.map((color, i) => (
+          <div key={i} css={colorBox(color)} />
+        ))}
+      </Anime>
+    </div>
   </>
 );
 
